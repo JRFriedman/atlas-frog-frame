3a0b2f5cd3f26e10c72a922d9597a0927a3cdb45
Jacob <jacob@joinatlas.xyz>
2024-05-02 19:43:20 -0400

    test image upload

--- api/index.tsx
+++ api/index.tsx
@@ -20,42 +20,44 @@ app.frame('/', (c) => {
   const { buttonValue, inputText, status } = c
   const input = inputText || buttonValue
   return c.res({
-    image: (
-      <div
-        style={{
-          alignItems: 'center',
-          background:
-            status === 'response'
-              ? 'linear-gradient(to right, white, #DDDDDD)'
-              : 'white',
-          backgroundSize: '100% 100%',
-          display: 'flex',
-          flexDirection: 'column',
-          flexWrap: 'nowrap',
-          height: '100%',
-          justifyContent: 'center',
-          textAlign: 'center',
-          width: '100%',
-        }}
-      >
-        <div
-          style={{
-            color: 'black',
-            fontSize: 60,
-            fontStyle: 'normal',
-            letterSpacing: '-0.025em',
-            lineHeight: 1.4,
-            marginTop: 30,
-            padding: '0 120px',
-            whiteSpace: 'pre-wrap',
-          }}
-        >
-          {status === 'response'
-            ? `Nice choice.${input ? ` ${input.toUpperCase()}!!` : ''}`
-            : 'Welcome to Atlas'}
-        </div>
-      </div>
-    ),
+    image: 'https://i.ibb.co/b18qNy5/atlas-frames.gif',
+    
+    // (
+    //   <div
+    //     style={{
+    //       alignItems: 'center',
+    //       background:
+    //         status === 'response'
+    //           ? 'linear-gradient(to right, white, #DDDDDD)'
+    //           : 'white',
+    //       backgroundSize: '100% 100%',
+    //       display: 'flex',
+    //       flexDirection: 'column',
+    //       flexWrap: 'nowrap',
+    //       height: '100%',
+    //       justifyContent: 'center',
+    //       textAlign: 'center',
+    //       width: '100%',
+    //     }}
+    //   >
+    //     <div
+    //       style={{
+    //         color: 'black',
+    //         fontSize: 60,
+    //         fontStyle: 'normal',
+    //         letterSpacing: '-0.025em',
+    //         lineHeight: 1.4,
+    //         marginTop: 30,
+    //         padding: '0 120px',
+    //         whiteSpace: 'pre-wrap',
+    //       }}
+    //     >
+    //       {status === 'response'
+    //         ? `Nice choice.${input ? ` ${input.toUpperCase()}!!` : ''}`
+    //         : 'Welcome to Atlas'}
+    //     </div>
+    //   </div>
+    // ),
     intents: [
       <TextInput placeholder="Ask Atlas..." />,
       <Button value="ideas">Ideas</Button>,
