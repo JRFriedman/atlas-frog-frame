4d8fb431be8b31a74e81ab9bcf6789fbf99f93e9
Jacob <jacob@joinatlas.xyz>
2024-05-02 20:13:10 -0400

    modify external links

--- api/index.tsx
+++ api/index.tsx
@@ -21,9 +21,9 @@ app.frame('/', (c) => {
     ),
     intents: [
       <TextInput placeholder="Ask Atlas..." />,
-      <Button.Link href="https://example.com/ideas">Ideas</Button.Link>, // Replace URL with desired external link
-      <Button.Link href="https://example.com/conversation">Conversations</Button.Link>, // Replace URL with desired external link
-      <Button.Link href="https://example.com/questions">Questions</Button.Link>, // Replace URL with desired external link
+      <Button.Link href="https://i.ibb.co/T88t6kW/Screen-Recording-2024-05-02-at-8-08-01-PM.gif">Ideas</Button.Link>, // Replace URL with desired external link
+      <Button.Link href="https://i.ibb.co/T88t6kW/Screen-Recording-2024-05-02-at-8-08-01-PM.gif">Conversations</Button.Link>, // Replace URL with desired external link
+      <Button.Link href="https://i.ibb.co/T88t6kW/Screen-Recording-2024-05-02-at-8-08-01-PM.gif">Questions</Button.Link>, // Replace URL with desired external link
       status === 'response' && <Button.Reset>Reset</Button.Reset>,
     ],
   })
